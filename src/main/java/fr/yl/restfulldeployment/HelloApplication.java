d11bacc7763a942fe0d00c11057ee2a5937e147b
Yoann-Lgd <yoann.lengrand@icloud.com>
2022-11-28 11:11:02 +0100

    First push

---
+++ src/main/java/fr/yl/restfulldeployment/HelloApplication.java
@@ -0,0 +1,9 @@
+package fr.yl.restfulldeployment;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
